fcae6a1d4faea7418a497d96e409aca58900fbc1
Bryan Joseph <bryan.joseph@sncorp.com>
2019-12-06 11:02:30 -0700

    First few sections of the course

--- CSharpPuzzlesProject/Program.cs
+++ CSharpPuzzlesProject/Program.cs
@@ -6,7 +6,83 @@ namespace CSharpPuzzlesProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // the factorial piece
+            // Console.WriteLine("Please enter the nth value as an integer:");
+            // var n = Convert.ToInt32(Console.ReadLine());
+
+            // call the loop
+            // Console.WriteLine($"Loop Calculation: Factorial of {n} is {CalculateLoop(n)}");
+            // call recursion
+            // Console.WriteLine($"Recursion Calculation: Factorial of {n} is {CalculateRecursion(n)}");
+
+            // the FizzBuzz piece
+            // Console.WriteLine("The loop solution for FizzBuzz:");
+            // first without recursion
+            //string text;
+            //for (int i = 1; i <= 100; i++)
+            //{
+            //    if (i % 3 == 0 && i % 5 == 0)
+            //    {
+            //        text = "FizzBuzz";
+            //    }
+            //    else if (i % 3 == 0)
+            //    {
+            //        text = "Fizz";
+            //    }
+            //    else if (i % 5 == 0)
+            //    {
+            //        text = "Buzz";
+            //    }
+            //    else text = i.ToString();
+
+            //    Console.WriteLine(text);
+            //}
+
+            // with recrusion
+            Console.WriteLine("The Recursive FizzBuzz Solution:");
+            FizzBuzzRecursion(1);
+        }
+
+        private static int CalculateLoop(int n)
+        {
+            var factorial = 1;
+            for (int i = n; i >= 1; i--)
+            {
+                factorial *= i;
+            }
+
+            return factorial;
+        }
+
+        private static int CalculateRecursion(int n)
+        {
+            if (n == 1) return 1;
+
+            return n * CalculateRecursion(n - 1);
+        }
+
+        private static void FizzBuzzRecursion(int n)
+        {
+            string text;
+            if (n > 100) return;
+
+            if (n % 15 == 0)
+            {
+                text = "FizzBuzz";
+            }
+            else if (n % 3 == 0)
+            {
+                text = "Fizz";
+            }
+            else if (n % 5 == 0)
+            {
+                text = "Buzz";
+            }
+            else text = n.ToString();
+
+            Console.WriteLine(text);
+
+            FizzBuzzRecursion(n + 1);
         }
     }
 }
